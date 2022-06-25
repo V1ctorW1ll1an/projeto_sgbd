06a9e24c65778e369c18f1f21d6a24163071df83
Victor Willian <victorwillian1012@gmail.com>
2022-06-25 14:18:22 -0300

    template inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php require("src/head.php"); ?>
+
+<body>
+
+    <?php require("src/navbar.php"); ?>
+
+    <main>
+        <?php require('./src/clientes.php'); ?>
+    </main>
+</body>
+
+</html>
---
+++ src/vendas.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require("head.php"); ?>
+
+<body>
+    <?php require("navbar.php"); ?>
+    <main>
+        Hello world!
+    </main>
+</body>
+
+</html>
